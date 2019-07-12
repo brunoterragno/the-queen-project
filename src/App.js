62a550d90477a34478b72e856a3c339c33f08ec7
Bruno Terragno <Bruno_Terragno@gap.com>
2019-07-12 11:55:08 -0300

    First :tada:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ingrid from "./images/ingrid.png";
+import bruno from "./images/bruno.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pictures: [ingrid, bruno],
+      users: [
+        { name: "Paty", surname: "Garcia" },
+        { name: "Andy", surname: "Garcia" },
+        { name: "Don", surname: "Garcia" }
+      ]
+    };
+  }
+
+  getName(user) {
+    return `${user.name} ${user.surname}`;
+  }
+
+  removeUser(removedUser) {
+    this.setState({
+      ...this.state,
+      users: this.state.users.filter(user => user !== removedUser)
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={this.state.pictures[Math.round(Math.random())]}
+            className="App-logo"
+            alt="logo"
+          />
+          <div className="App-list">
+            {this.state.users.map(user => (
+              <p className="App-list-item">
+                {this.getName(user)}
+                <button
+                  className="App-list-item-button"
+                  onClick={() => this.removeUser(user)}
+                >
+                  Remove
+                </button>
+              </p>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
