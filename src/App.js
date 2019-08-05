0f1875af32e650619c07429f49587fbe13e60f2b
Ingrid Murielem <murielem.ingrid@gmail.com>
2019-08-05 16:48:11 -0300

    Adicionado botão

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import Image from "./components/Image.js";
 import Users from "./components/Users.js"
 
 class App extends React.Component {
-  removeUser(removedUser) {
-    this.setState({
-      ...this.state,
-      users: this.state.users.filter(user => user !== removedUser)
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Image teste={true}/>
+          <Image/>
           <p>
-             <Users teste={true} />
+             <Users/>
           </p>
         </header>
       </div>
--- src/components/Users.js
+++ src/components/Users.js
@@ -13,10 +13,16 @@ class Users extends React.Component{
      getName(user) {
          return `${user.name} ${user.surname}`;
         }
+      removeUser(removedUser) {
+        this.setState({
+          ...this.state,
+          users: this.state.users.filter(user => user !== removedUser)
+        });
+      }
      render (){
          return (
             <div className="App-list">{this.state.users.map(user => (<p className="App-list-item">{this.getName(user)}
-              </p>))}
+            <button className="App-list-item-button" onClick={() => this.removeUser(user)}>Remove</button></p>))}
             </div>
          ) 
      }
