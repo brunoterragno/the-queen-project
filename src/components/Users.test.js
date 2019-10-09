305c303a52f5a29cf6509fa84add77b5a83abbaf
Ingrid Murielem <murielem.ingrid@gmail.com>
2019-10-09 10:54:46 -0300

    wip

--- src/components/Users.test.js
+++ src/components/Users.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Users from "./Users"
+import Users from "./Users";
+import render from '@testing-library/react';
 
 it('renders without crashing', () => {
     const list = []
@@ -8,6 +9,17 @@ it('renders without crashing', () => {
     ReactDOM.render(<Users users={list} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+    describe ("User", () => {
+      it("should return user list if type is user", () => {
+        const component = render(<Users type= {'users'} users={[]}/>)
+        expect(component).toEqual({})
+
+
+      });
+        
+    });
+
 
   // recebe uma lista  e mostra a lista
   // recebe duas funções: Add e remove e o tipo(user ou admin)
